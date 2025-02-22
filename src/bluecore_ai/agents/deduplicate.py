d5452ab72ac48a74a045a1072bd5db25da283981
Jeremy Nelson <jermnelson@gmail.com>
2025-02-21 16:28:27 -0800

    feat: New de-duplication Agent

---
+++ src/bluecore_ai/agents/deduplicate.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Union
+
+from pydantic_ai import Agent, RunContext
+
+
+@dataclass
+class SupportDependencies:
+    model: str
+    bluecore_api_url: str
+
+
+agent = Agent(
+    result_type=bool,
+    system_prompt=(
+        'You are an expert on deciding if a BIBFRAME Work or Instance '
+        'already exists in our datastore. '
+    )
+)
+
+@agent.tool
+async def is_duplicate(
+    ctx: RunContext[SupportDependencies]
+) -> Union[bool, float]:
+    return True
+    
