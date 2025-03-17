0cc6ca7b7b78daa4483520558a3e6fa4f8e70a55
Jeremy Nelson <jermnelson@gmail.com>
2025-03-17 08:23:16 -0700

    feat: New Validation Agent with updated dependencies

--- src/bluecore_ai/agents/validation.py
+++ src/bluecore_ai/agents/validation.py
@@ -1,8 +1,38 @@
-"""Module for a Validation Generative AI Agent for Blue Core"
-import dataclass
+"""Module for a Validation Generative AI Agent for Blue Core"""
+
 import pathlib
 
+
+from dataclass import dataclass
+from typing import Union
+
 import rdflib
+
+from pydantic_ai import Agent, RunContext
+from pyshacl import validate
+
+
 @dataclass
-class SHACL:
-    graph: rdflib.Graph
+class SupportDependencies:
+    model: str
+    bluecore_api_url: str
+    shacl: Union[rdflib.Graph, None]
+
+
+agent = Agent(
+    result_type=bool,
+    system_prompt=(
+        'You are an expert on deciding if a BIBFRAME Work or Instance '
+        'is valid according to the SHACL graphs in the Blue Core vector'
+        'space. A critical test is the {validate} tool.'
+    )
+)
+
+@agent.tool
+async def validate(
+    ctx: RunContext[SupportDependencies]
+) -> Union[bool, rdflib.Graph]:
+    return True
+    
+
+
